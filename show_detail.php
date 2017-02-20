2dcc489b348387b898cdef42970c9e9c24ac58ba
Kudo <phuangpila101@hotmail.com>
2017-02-20 17:46:09 +0700

    gg2
    gg2

--- show_detail.php
+++ show_detail.php
@@ -5,42 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>นพเกล้าเภสัช</title>
 	<!-- BOOTSTRAP STYLES-->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="include/assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FONTAWESOME STYLES-->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="include/assets/css/font-awesome.css" rel="stylesheet" />
         <!-- CUSTOM STYLES-->
-    <link href="assets/css/custom.css" rel="stylesheet" />
+    <link href="include/assets/css/custom.css" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
- <link href='assets/css/sens.css' rel='stylesheet' type='text/css' />
+ <link href='include/assets/css/sens.css' rel='stylesheet' type='text/css' />
 </head>
 <body>
-     
-           
-          
-    <div id="wrapper">
-         <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="adjust-nav">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">
-                        <img src="assets/img/logo.png" />
-
-                    </a>
-                    
-                </div>
-              
-                <span class="logout-spn" >
-                  <a href="#" style="color:#fff;">ออกจากระบบ</a>  
-
-                </span>
-            </div>
-        </div>
-        
- <br>
+     <?php include("include/comtop.php"); ?>
                 <div class="row">
                     <div class="col-lg-12">
                      <h2>&nbsp;นพเกล้าเภสัช</h2>   
@@ -181,24 +155,17 @@
                   </div> 
               </div>   
         </div>
-        <br><br><br><br><br><br><br><br><br>
-    <div class="footer">
-            <div class="row">
-                <div class="col-lg-12" >
-                   &copy; 2017
-                </div>
-            </div>
-        </div>
+    
           
-
+<?php include("include/combuttom.php"); ?>
      <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
+    <script src="include/assets/js/jquery-1.10.2.js"></script>
       <!-- BOOTSTRAP SCRIPTS -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="include/assets/js/bootstrap.min.js"></script>
       <!-- CUSTOM SCRIPTS -->
-    <script src="assets/js/custom.js"></script>
+    <script src="include/assets/js/custom.js"></script>
     
    
 </body>
