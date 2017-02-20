05aff8bd5f1104eac282cdfbf8eeca3b829b6c7a
Kudo <phuangpila101@hotmail.com>
2017-02-20 17:52:26 +0700

    Revert "gg3"
    This reverts commit cef39082f92b4100b31ea7a37296ddbbbc48ec1f.

--- include/comtop.php
+++ include/comtop.php
@@ -5,65 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Simple Responsive Admin</title>
   <!-- BOOTSTRAP STYLES-->
-    <link href="include/assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
      <!-- FONTAWESOME STYLES-->
-    <link href="include/assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
         <!-- CUSTOM STYLES-->
-    <link href="include/assets/css/custom.css" rel="stylesheet" />
+    <link href="assets/css/custom.css" rel="stylesheet" />
      <!-- GOOGLE FONTS-->
-   <link href='include/assets/css/sens.css' rel='stylesheet' type='text/css' />
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 </head>
-<style type="text/css">
-html, body { 
-   height: 100%; /* ให้ html และ body สูงเต็มจอภาพไว้ก่อน */
-   margin: 0;
-   padding: 0;
-}
-.navbar {
-background: #0264d6; /* Old browsers */
-background: -moz-radial-gradient(center, ellipse cover,  #0264d6 1%, #1c2b5a 100%); /* FF3.6+ */
-background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(1%,#0264d6), color-stop(100%,#1c2b5a)); /* Chrome,Safari4+ */
-background: -webkit-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Chrome10+,Safari5.1+ */
-background: -o-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Opera 12+ */
-background: -ms-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* IE10+ */
-background: radial-gradient(ellipse at center,  #0264d6 1%,#1c2b5a 100%);
-}
 
-</style>
 <body>
-      <div id="wrapper">
-         <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="adjust-nav">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">
-                        <img src="include/assets/img/logo.png" />
-
-                    </a>
-                    
-                </div>
-              
-                <span class="logout-spn" >
-                  <a href="index.php" style="color:#fff;">ออกจากระบบ</a>  
-
-                </span>
-            </div>
-        </div>
-        
- <br>
+     
+           
+   <div id="wrapper">
+           <div class="navbar navbar-inverse navbar-fixed-top">
+              <div class="adjust-nav">
+                  <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#">
+                          <img src="assets/img/logo.png" />
+            
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
       
      <!-- /. WRAPPER  -->
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
-    <script src="include/assets/js/jquery-1.10.2.js"></script>
+    <script src="assets/js/jquery-1.10.2.js"></script>
       <!-- BOOTSTRAP SCRIPTS -->
-    <script src="include/assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
       <!-- CUSTOM SCRIPTS -->
-    <script src="include/assets/js/custom.js"></script>
+    <script src="assets/js/custom.js"></script>
     
    
 </body>
