e5c79777399f14d01038f071a8901f34a6629083
taweechai <thekop11642@gmail.com>
2017-02-21 13:21:46 +0700

    s

--- insert_type_derug.php
+++ insert_type_derug.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<form action="save_type_derug.php" method="post" name="form1">
+<form action="#" method="post" name="form1">
 		<div class="row">
 			<div>			
 				<p><h3>เพิ่มข้อมูลยา</h3></p>
