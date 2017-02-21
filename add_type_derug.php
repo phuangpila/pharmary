f6b6615432b09945237e37d5a681ced0c320b2ed
taweechai <thekop11642@gmail.com>
2017-02-21 09:40:21 +0700

    add

--- add_type_derug.php
+++ add_type_derug.php
@@ -14,6 +14,24 @@
 		<div>
 			<button class="btn btn-success" onclick="window.open('../pharmary/insert_type_derug.php','mywindow','location=1,status=1,scrollbars=1,width=800,height=400');">เพิ่มข้อมูล</button>
 		</div>
+		<div class="span-12">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>ลำดับ</th>
+						<th>ประเภทยา</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </body>
 </html>
