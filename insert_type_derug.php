c539db669fce011b3d62c7763c7f87a40472d167
taweechai <thekop11642@gmail.com>
2017-02-21 09:37:45 +0700

    w

--- insert_type_derug.php
+++ insert_type_derug.php
@@ -6,24 +6,27 @@
 	<title>Document</title>
 </head>
 <body>
+	<form action="" method="" name="">
+
 		<div class="row">
 			<div>
-				<form action="" method="" name="">
-					<p><h3>เพิ่มข้อมูลยา</h3></p>
-					<hr>
-					<table align="center">
-							<tr>
-								<td>ประเภทยา : </td>
-								<td>
-									<input type="text" class="form-control" id="" name="">
-								</td>							
-								<td align="right" style="width: 15%">
-									<input type="submit" name="" value="บันทึก" class="btn btn-success">
-								</td>
-							</tr>
-					</table>
-				</form>		
-			</div>	
+			<hr>		
+				<table align="center">
+						<tr>
+							<td>ประเภทยา : <span class="f_req"></span></td>
+							<td>
+								<input type="text" name="" id="" value="" class="form-control">
+							</td>
+						</tr>
+				</table>					
+			</div><br>
+			<div align="center">
+				<div>
+					<input type="button" name="btnSave" id="btnSave" class="btn btn-small btn-success" value="บันทึก" />
+					<input type="button" class="btn btn-small btn-danger" value="ปิด" onclick="window.close();">
+				</div>
+			</div>
 		</div>
+	</form>
 </body>
 </html>
