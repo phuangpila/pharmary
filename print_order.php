37e175a4fa55c39920c3bbcebf546ef00aaa8109
taweechai <thekop11642@gmail.com>
2017-03-01 15:03:53 +0700

    gg

--- print_order.php
+++ print_order.php
@@ -24,7 +24,7 @@ $pdf->SetFont('angsana','',14);
  
   $pdf->Cell(50,5,'',0,1,'C');
   $pdf->Cell(50,5,'',0,0,'C');
-  $pdf->Cell(75 ,10, iconv( 'UTF-8','cp874' ,'ใบเสร็จรับเงิน/ใบกำกับภาษี'),1,1,'C');
+  $pdf->Cell(75 ,10, iconv( 'UTF-8','cp874' ,'ใบสั่งสินค้า'),1,1,'C');
   $pdf->Cell(50,5,'',0,1,'C',0);//เว้นบรรทัด
 
   $pdf->Cell(20 , 8, iconv( 'UTF-8','cp874' ,'ชื่อร้าน : '),0,0,'L' ); 
@@ -57,31 +57,6 @@ $pdf->SetFont('angsana','',14);
   $pdf->Cell(60 , 8, iconv( 'UTF-8','cp874' ,'Fax : '),0,0,'R' );
   $pdf->Cell(90 , 8, iconv( 'UTF-8','cp874' ,$res_sup['supp_fax']),0,1,'' );
 
-/*
-  $pdf->Cell(25,8,iconv( 'UTF-8','cp874' ,'บริษัทที่สั่งซื้อ : '),0,0,'L' );
-  $pdf->Cell(110,8,iconv( 'UTF-8','cp874' ,$res_sup['supp_name']),0,0,'' );
-
-  $pdf->Cell(20 , 8, iconv( 'UTF-8','cp874' ,'รหัสการสั่งซื้อสินค้า : '),0,0,'R' );  
-  $pdf->Cell(40 , 8, iconv( 'UTF-8','cp874' ,$_GET['od_id']),0,1,'' );
-
-  $pdf->Cell(25 , 8, iconv( 'UTF-8','cp874' ,'ที่อยู่ : '),0,0,'L' );
-  $pdf->Cell(110 , 8, iconv( 'UTF-8','cp874' ,$res_sup['supp_add']),0,0,'' );
-
-  $pdf->Cell(20 , 8, iconv( 'UTF-8','cp874' ,'Email : '),0,0,'R' );  
-  $pdf->Cell(40 , 8, iconv( 'UTF-8','cp874' ,$res_sup['supp_email']),0,1,'' );
-
-    $pdf->Cell(25 , 8, iconv( 'UTF-8','cp874' ,'เบอร์โทร : '),0,0,'L' );
-  $pdf->Cell(110 , 8, iconv( 'UTF-8','cp874' ,$res_sup['supp_tel']),0,0,'' );
-
-  $pdf->Cell(20 , 8, iconv( 'UTF-8','cp874' ,'Fax : '),0,0,'R' );  
-  $pdf->Cell(40 , 8, iconv( 'UTF-8','cp874' ,$res_sup['supp_fax']),0,1,'' );
-
-
-  $pdf->Cell(20,8,iconv( 'UTF-8','cp874' ,'วันที่สั่งสินค้า : '),0,0,'L' );
-  $pdf->Cell(110,8,iconv( 'UTF-8','cp874' ,$res_order['date_rec']),0,1,'' );
-*/
-
-
 
    $pdf->Cell(0,1,'',0,1,'C');
  $pdf->Cell(15,15,iconv( 'UTF-8','cp874' ,'รายการ'),1,0,'C',0);
@@ -96,7 +71,7 @@ $pdf->Cell(35,15,iconv( 'UTF-8','cp874' ,'ราคารวม (บาท)'),1,
             while ($res=mysql_fetch_array($query)) {
 
                 $pdf->Cell(15,10,$i++,1,0,'C',0);
-                $pdf->Cell(90 ,10, iconv( 'UTF-8','cp874' ,$res['od_name']),0,0,'C' );
+                $pdf->Cell(90 ,10, iconv( 'UTF-8','cp874' ,$res['od_name']),1,0,'C' );
                 $pdf->Cell(15,10,$res['od_unit'],1,0,'C',0);
                 $pdf->Cell(35,10,$res['od_price'],1,0,'C',0);
                 $pdf->Cell(35,10,$res['od_price']*$res['od_unit'],1,1,'C',0);
@@ -109,7 +84,7 @@ $pdf->Cell(35,15,iconv( 'UTF-8','cp874' ,'ราคารวม (บาท)'),1,
             $pdf->Cell(20,5,'',0,0,'C',0);
 
   $pdf->Cell(20 , 8, iconv( 'UTF-8','cp874' ,'ชื่อผู้ส่งสินค้า '),0,0,'L' );  
-  $pdf->Cell(130 , 8, iconv( 'UTF-8','cp874' ,'ชื่อผู้รับสินค้า '),0,1,'R' );  
+  $pdf->Cell(130 , 8, iconv( 'UTF-8','cp874' ,'ชื่อผู้รับเงิน '),0,1,'R' );  
   $pdf->Cell(0, 8, iconv( 'UTF-8','cp874' ,'...................................................................'),0,0,'L' );
   $pdf->Cell(0, 8, iconv( 'UTF-8','cp874' ,'.................................................................'),0,1,'R' );
   $pdf->Cell(0, 8, iconv( 'UTF-8','cp874' ,'(..................................................................)'),0,0,'L' );
